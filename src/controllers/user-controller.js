c8f99735d252281bbe42166bda172674773d3387
Ilya Siz <i.siz@godeltech.com>
2024-03-13 10:46:45 +0100

    PROXY-11: address pull request comment

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -5,8 +5,7 @@ const postUser = async (req, res, next) => {
     const request = mapQueryToUserRequest(req.query);
 
     try {
-        // eslint-disable-next-line no-unused-vars
-        const userDataProcessed = await userService.processUserData(request);
+        await userService.processUserData(request);
         const roverPhotoUrl = await roverPhotoService.getRoverPhotoUrl();
         res.send(`<img src="${roverPhotoUrl}">`);
     } catch (error) {
